bf5cce5d4d73726f80fd711af9f32a6f8b5c18cf
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-07-13 03:21:12 +0800

    chore: add dashjs in the examples

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,12 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { UncontrolledLionPlyr, useHlsPlyr, usePlyr } from '../.';
+import {
+  UncontrolledLionPlyr,
+  useHlsPlyr,
+  useDashPlyr,
+  usePlyr,
+} from '../.';
 
 const videoSrc = {
   type: 'video',
@@ -20,6 +25,16 @@ const hlsSrc = {
       src:
         'https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8',
       type: 'application/x-mpegURL'
+    }
+  ]
+};
+
+const dashSrc = {
+  type: 'video',
+  sources: [
+    {
+      src: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
+      type: 'application/dash+xml',
     }
   ]
 };
@@ -71,13 +86,63 @@ const YoutubeApp = () => {
   );
 }
 
-const App = () => {
-  const [toggle, setToggle] = React.useState(true);
+const DashApp = () => {
+  const dashRef = useDashPlyr({
+    source: dashSrc,
+  });
+
+  React.useEffect(() => {
+    const dashPlayer = dashRef.current.plyr;
+
+    if (dashPlayer) {
+      dashPlayer.on('timeupdate', event => {
+        const instance = event.detail.plyr;
+        console.log(instance.currentTime);
+      });
+    }
+  })
 
   return (
     <div>
-      {toggle ? <HlsApp /> : <YoutubeApp />}
-      <button onClick={() => setToggle(currentState => !currentState)}>Toggle</button>
+      <h1>Dash Usage</h1>
+      <UncontrolledLionPlyr ref={dashRef} />
+    </div>
+  );
+}
+
+const App = () => {
+  const [state, setState] = React.useState('hls');
+
+  return (
+    <div>
+      {
+        state === 'hls' && (
+          <HlsApp />
+        )
+      }
+      {
+        state === 'youtube' && (
+          <YoutubeApp />
+        )
+      }
+      {
+        state === 'dash' && (
+          <DashApp />
+        )
+      }
+      <div style={
+        {
+          display: 'grid',
+          gridTemplateColumns: 'repeat(3, 1fr)',
+          marginTop: '16px',
+          gridColumnGap: '16px',
+          height: '40px',
+        }
+      }>
+        <button onClick={() => setState('hls')}>HLS Player</button>
+        <button onClick={() => setState('youtube')}>Youtube Player</button>
+        <button onClick={() => setState('dash')}>Dash Player</button>
+      </div>
     </div>
   );
 };
