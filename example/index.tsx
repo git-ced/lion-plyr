8661c7f7c8fbc77b48d0a80409d9f2da0b60beee
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-07-16 05:22:08 +0800

    chore: add a MP4 example

--- example/index.tsx
+++ example/index.tsx
@@ -39,6 +39,22 @@ const dashSrc = {
   ]
 };
 
+const mp4Src = {
+  type: 'video',
+  sources: [
+    {
+      src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4',
+      type: 'video/mp4',
+      size: 720,
+    },
+    {
+      src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4',
+      type: 'video/mp4',
+      size: 1080,
+    }
+  ]
+};
+
 const HlsApp = () => {
   const hlsRef = useHlsPlyr({
     source: hlsSrc,
@@ -86,6 +102,29 @@ const YoutubeApp = () => {
   );
 }
 
+const Mp4App = () => {
+  const mp4Ref = usePlyr({
+    source: mp4Src,
+  });
+  React.useEffect(() => {
+    const mp4Player = mp4Ref.current?.plyr;
+
+    if (mp4Player) {
+      mp4Player.on('timeupdate', event => {
+        const instance = event.detail.plyr;
+        console.log(instance.currentTime);
+      });
+    }
+  })
+
+  return (
+    <div>
+      <h1>MP4 Usage</h1>
+      <UncontrolledLionPlyr ref={mp4Ref} />
+    </div>
+  );
+}
+
 const DashApp = () => {
   const dashRef = useDashPlyr({
     source: dashSrc,
@@ -111,18 +150,18 @@ const DashApp = () => {
 }
 
 const App = () => {
-  const [state, setState] = React.useState('hls');
+  const [state, setState] = React.useState('mp4');
 
   return (
     <div>
       {
-        state === 'hls' && (
-          <HlsApp />
+        state === 'mp4' && (
+          <Mp4App />
         )
       }
       {
-        state === 'youtube' && (
-          <YoutubeApp />
+        state === 'hls' && (
+          <HlsApp />
         )
       }
       {
@@ -130,18 +169,24 @@ const App = () => {
           <DashApp />
         )
       }
+      {
+        state === 'youtube' && (
+          <YoutubeApp />
+        )
+      }
       <div style={
         {
           display: 'grid',
-          gridTemplateColumns: 'repeat(3, 1fr)',
+          gridTemplateColumns: 'repeat(4, 1fr)',
           marginTop: '16px',
           gridColumnGap: '16px',
           height: '40px',
         }
       }>
+        <button onClick={() => setState('mp4')}>MP4 Player</button>
         <button onClick={() => setState('hls')}>HLS Player</button>
-        <button onClick={() => setState('youtube')}>Youtube Player</button>
         <button onClick={() => setState('dash')}>Dash Player</button>
+        <button onClick={() => setState('youtube')}>Youtube Player</button>
       </div>
     </div>
   );
